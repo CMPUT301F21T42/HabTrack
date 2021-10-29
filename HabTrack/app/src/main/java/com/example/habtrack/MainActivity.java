0a05983604de1f5f146dbb1de99782efa996cdcf
Wendy Zhang <qianyu1@ualberta.ca>
2021-10-29 09:41:23 -0600

    linked user profile activity

--- HabTrack/app/src/main/java/com/example/habtrack/MainActivity.java
+++ HabTrack/app/src/main/java/com/example/habtrack/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.habtrack;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.Toast;
@@ -30,7 +32,8 @@ import java.util.HashMap;
 import com.example.habtrack.ui.edithabit.EdithabitFragment;
 
 public class MainActivity extends AppCompatActivity
-        implements EdithabitFragment.OnFragmentInteractionListener{
+        implements NavigationView.OnNavigationItemSelectedListener,
+        EdithabitFragment.OnFragmentInteractionListener{
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
@@ -55,13 +58,15 @@ public class MainActivity extends AppCompatActivity
 
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_myday, R.id.nav_friends, R.id.nav_manage, R.id.nav_events,
-                R.id.nav_profile, R.id.nav_following, R.id.nav_follower)
+                R.id.nav_following, R.id.nav_follower)
                 .setOpenableLayout(drawer)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
@@ -69,6 +74,7 @@ public class MainActivity extends AppCompatActivity
         BottomNavigationView bottom_nav_view = findViewById(R.id.bottom_nav_view);
         NavigationUI.setupWithNavController(bottom_nav_view, navController);
 
+        navigationView.setNavigationItemSelectedListener(this);
         navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
             public void onDestinationChanged(NavController controller, NavDestination destination, Bundle arguments) {
                 getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_person_24);
@@ -88,6 +94,15 @@ public class MainActivity extends AppCompatActivity
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.nav_profile) {
+            Intent intent = new Intent(this, UserProfileActivity.class);
+            startActivity(intent);
+        }
+        return true;
     }
 
     @Override
