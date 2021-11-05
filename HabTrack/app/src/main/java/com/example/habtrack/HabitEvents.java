cd555bdbb0314288d0f21845a24e2f9e760dd678
revanthavs <atmakurirevanth@gmail.com>
2021-11-05 13:39:57 -0600

    Made some changes

--- HabTrack/app/src/main/java/com/example/habtrack/HabitEvents.java
+++ HabTrack/app/src/main/java/com/example/habtrack/HabitEvents.java
@@ -13,11 +13,12 @@
  */
 package com.example.habtrack;
 
+import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-public class HabitEvents {
+public class HabitEvents implements Serializable {
     // TODO: Need to modify Habit attribute if it's efficient to just store the habit title
 
     private String title;   // Storing the title of HabitEvent
@@ -29,9 +30,7 @@ public class HabitEvents {
     String timeStamp;
     String HabitEventID;
 
-    HabitEvents() {
-
-    }
+    public HabitEvents() {}
 
     public String getHabitEventID() {
         return HabitEventID;
@@ -97,3 +96,103 @@ public class HabitEvents {
         this.location = location;
     }
 }
+
+///** Copyright 2021
+// * Raunak Agarwal, Revanth Atmakuri, Mattheas Jamieson,
+// * Jenish Patel, Jasmine Wadhwa, Wendy Zhang
+// * Licensed under the Apache License, Version 2.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * You may obtain a copy of the License at
+// *
+// * http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * CMPUT301F21T42 Project: HabTrack <br>
+// * To help someone who wants to track their habits.
+// * The {@code Habit} class
+// */
+//package com.example.habtrack;
+//
+//import java.text.SimpleDateFormat;
+//import java.util.Calendar;
+//import java.util.Date;
+//
+//public class HabitEvents {
+//    // TODO: Need to modify Habit attribute if it's efficient to just store the habit title
+//
+//    private String title;   // Storing the title of HabitEvent
+//    private String comment; // Storing the user comment on HabitEvent
+//
+//    // TODO: Should modify these attributes for Project part 4
+//    private Boolean Photo;
+//    private Boolean location;
+//    String timeStamp;
+//    String HabitEventID;
+//
+//    HabitEvents() {
+//
+//    }
+//
+//    public String getHabitEventID() {
+//        return HabitEventID;
+//    }
+//
+//    public void setHabitEventID(String HabitEventID) {
+//        this.HabitEventID = HabitEventID;
+//    }
+//
+//    public String getTimeStamp() {
+//        return timeStamp;
+//    }
+//
+//    public void setTimeStamp(String timeStamp) {
+//        this.timeStamp = timeStamp;
+//    }
+//
+//    // TODO: Need to add default values for Photo and location
+//    public HabitEvents(String habitTitle, String comment, Boolean Photo,
+//                       Boolean location, String timeStamp) {
+//
+//        // TODO: Need to type check all the attributes
+//
+//        this.title = habitTitle;
+//        this.comment = comment;
+//        this.Photo = Photo;
+//        this.location = location;
+//        this.timeStamp = timeStamp;
+//        this.HabitEventID = title+timeStamp;
+//    }
+//
+//
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getComment() {
+//        return comment;
+//    }
+//
+//    public void setComment(String comment) {
+//        this.comment = comment;
+//    }
+//
+//    public Boolean getPhoto() {
+//        return Photo;
+//    }
+//
+//    public void setPhoto(Boolean photo) {
+//        Photo = photo;
+//    }
+//
+//    public Boolean getLocation() {
+//        return location;
+//    }
+//
+//    public void setLocation(Boolean location) {
+//        this.location = location;
+//    }
+//}
--- HabTrack/app/src/main/java/com/example/habtrack/ui/events/EventsFragment.java
+++ HabTrack/app/src/main/java/com/example/habtrack/ui/events/EventsFragment.java
@@ -1,57 +1,59 @@
 package com.example.habtrack.ui.events;
 
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.habtrack.EventsAdapter;
+import com.example.habtrack.Habit;
 import com.example.habtrack.HabitEvents;
-import com.example.habtrack.R;
-//import com.example.habtrack.ui.events.ViewEditDeleteHabitEvent;
+import com.example.habtrack.OnItemClickListener;
+import com.example.habtrack.databinding.FragmentEventsBinding;
+import com.example.habtrack.ui.edithabit.EdithabitFragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.ArrayList;
 
-//AddHabitEventFragment.OnFragmentInteractionListener
-public class EventsFragment extends Fragment{
+public class EventsFragment extends Fragment {
 
-    ListView EventsList;
-    ArrayAdapter<HabitEvents> eventAdapter;
-    ArrayList<HabitEvents> eventsDataList;
+    //    private EventsViewModel eventsViewModel;
+    private FragmentEventsBinding binding;
 
-    public EventsFragment() {
-        // Required empty public constructor
-    }
+    private ListView eventList;
+    private ArrayList<HabitEvents> dataList;
+    private ArrayAdapter<HabitEvents> eventAdapter;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    FirebaseFirestore db;
 
-        View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_events, null);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+//        eventsViewModel =
+//                new ViewModelProvider(this).get(EventsViewModel.class);
 
-        EventsList = view.findViewById(R.id.eventList); // To display the Habit Events lists
+        binding = FragmentEventsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-        eventsDataList = new ArrayList<>();
+        eventList = binding.eventList;
+        dataList = new ArrayList<>();
 
-
-        // TODO: Need to check if it's getContext or getActivity
-
-        eventAdapter = new EventsAdapter(getContext(), eventsDataList);
-
-        EventsList.setAdapter(eventAdapter);
-
+        eventAdapter = new EventsAdapter(getContext(), dataList);
+        eventList.setAdapter(eventAdapter);
 
         FirebaseFirestore HabTrackDB = FirebaseFirestore.getInstance();
         HabTrackDB.collection("Users")
@@ -60,34 +62,289 @@ public class EventsFragment extends Fragment{
                 .addSnapshotListener(new EventListener<QuerySnapshot>() {
                     @Override
                     public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                        eventsDataList.clear();
+                        dataList.clear();
                         for(QueryDocumentSnapshot hevent: value) {
                             HabitEvents hEvent = hevent.toObject(HabitEvents.class);
-//                            eventsDataList.add(hEvent);
-                            eventAdapter.add(hEvent);
+                            dataList.add(hEvent);
                         }
                         eventAdapter.notifyDataSetChanged();
                     }
                 });
 
-        EventsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                HabitEvents currentEvent = eventAdapter.getItem(i);
-
-                new ViewEditDeleteHabitEvent(currentEvent);
-            }
-        });
-
+        return root;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_events, container, false);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
-
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//package com.example.habtrack.ui.events;
+//
+//
+//import android.media.MediaPlayer;
+//import android.os.Bundle;
+//import androidx.annotation.NonNull;
+//import androidx.annotation.Nullable;
+//import androidx.fragment.app.Fragment;
+//import android.util.Log;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.AdapterView;
+//
+//import com.example.habtrack.EventsAdapter;
+//import com.example.habtrack.HabitEvents;
+//import android.widget.ArrayAdapter;
+//import android.widget.ListView;
+//
+//import com.example.habtrack.R;
+//import com.example.habtrack.ViewEditDeleteHabitEvent;
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.firestore.CollectionReference;
+//import com.google.firebase.firestore.EventListener;
+//import com.google.firebase.firestore.FirebaseFirestore;
+//import com.google.firebase.firestore.FirebaseFirestoreException;
+//import com.google.firebase.firestore.QueryDocumentSnapshot;
+//import com.google.firebase.firestore.QuerySnapshot;
+//import java.util.ArrayList;
+//
+////AddHabitEventFragment.OnFragmentInteractionListener
+//public class EventsFragment extends Fragment{
+//
+//    ListView EventsList;
+//    ArrayAdapter<HabitEvents> eventAdapter;
+//    ArrayList<HabitEvents> eventsDataList;
+//
+//    public EventsFragment() {
+//        // Required empty public constructor
+//    }
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_events, null);
+//
+//        EventsList = view.findViewById(R.id.eventList); // To display the Habit Events lists
+//
+//        eventsDataList = new ArrayList<>();
+//
+//        // TODO: Need to check if it's getContext or getActivity
+//        eventAdapter = new EventsAdapter(getContext(), eventsDataList);
+//
+//        EventsList.setAdapter(eventAdapter);
+//
+//        EventsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//
+//                HabitEvents currentEvent = eventAdapter.getItem(i);
+//
+//                new ViewEditDeleteHabitEvent(currentEvent);
+//            }
+//        });
+//
+//        FirebaseFirestore HabTrackDB = FirebaseFirestore.getInstance();
+//        HabTrackDB.collection("Users")
+//                .document("User1")
+//                .collection("HabitEvents")
+//                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+//                        eventsDataList.clear();
+//                        for(QueryDocumentSnapshot hevent: value) {
+//                            HabitEvents hEvent = hevent.toObject(HabitEvents.class);
+//                        }
+//                        eventAdapter.notifyDataSetChanged();
+//                    }
+//                });
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_events, container, false);
+//    }
+//
+//}
+//
+//
+//
+//
+////    public void addToHabitEventClass(DataSnapshot snapshot) {
+////        HabitEvents newHabitEvent = (HabitEvents) snapshot.getValue();
+////        eventAdapter.add(newHabitEvent);
+////    }
+////
+////    @Override
+////    public void onOkPressed(String habitEventTitle, String comment, Boolean photo, Boolean location){
+////
+////        // The Id for this new HabitEvent would be "Workout20210929" if title for habit is "Workout"
+////        // and if the date user finished this habit is 2021/09/29
+////        String timestamp = new SimpleDateFormat("yyyy/MM/dd").format(Calendar.getInstance().getTime());
+////        String HabitEventId = habitEventTitle + timestamp;
+////
+////        DatabaseReference newHabitEvent = HabitEventsTable.child(HabitEventId);
+////        newHabitEvent.child("Title").setValue(habitEventTitle);
+////        newHabitEvent.child("Comment").setValue(comment);
+////        // Temporary place holders for photo and location
+////        newHabitEvent.child("Photo").setValue(false);
+////        newHabitEvent.child("Location").setValue(false);
+////        newHabitEvent.child("TimeStamp").setValue(timestamp);
+////        Log.d("Firestore", "new HabitEvent is added to FireStore");
+////
+////        // TODO: Need to use only one DatabaseReference
+////        DatabaseReference ref = FirebaseDatabase.getInstance()
+////                .getReference(path+"/HabitEvents/"+habitEventTitle+timestamp);
+////
+////        ref.addValueEventListener(new ValueEventListener() {
+////            @Override
+////            public void onDataChange(@NonNull DataSnapshot snapshot) {
+////                addToHabitEventClass(snapshot);
+////                Log.d("Firestore", "Got data back");
+////            }
+////
+////            @Override
+////            public void onCancelled(@NonNull DatabaseError error) {
+////
+////            }
+////        });
+////
+//////        eventAdapter.add(new HabitEvents(habitEventTitle, comment, photo, location));
+////    }
+//
+////    private void addDataToEventAdapter() {
+////        String path = "Users/" + FirebaseAuth.getInstance().getCurrentUser().getUid()+"/HabitEvents";
+////
+////        DatabaseReference HEref = FirebaseDatabase.getInstance().getReference(path);
+////
+////        // DATA: [ {HabitEventID1: TITLE, COMMENT, PHOTO, LOCATION, HabitEventID},
+//////                  {HabitEventID1: TITLE, COMMENT, PHOTO, LOCATION, HabitEventID}]
+////
+////        Query HElist = HEref.orderByKey();
+////
+////        HElist.addValueEventListener(new ValueEventListener() {
+////            @Override
+////            public void onDataChange(@NonNull DataSnapshot snapshot) {
+////
+////            }
+////
+////            @Override
+////            public void onCancelled(@NonNull DatabaseError error) {
+////
+////            }
+////        });
+////
+////    }
+
+
+//package com.example.habtrack.ui.events;
+//
+//import android.os.Bundle;
+//
+//import androidx.annotation.Nullable;
+//import androidx.fragment.app.Fragment;
+//
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.AdapterView;
+//import android.widget.ArrayAdapter;
+//import android.widget.ListView;
+//
+//import com.example.habtrack.EventsAdapter;
+//import com.example.habtrack.HabitEvents;
+//import com.example.habtrack.R;
+////import com.example.habtrack.ui.events.ViewEditDeleteHabitEvent;
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.firestore.EventListener;
+//import com.google.firebase.firestore.FirebaseFirestore;
+//import com.google.firebase.firestore.FirebaseFirestoreException;
+//import com.google.firebase.firestore.QueryDocumentSnapshot;
+//import com.google.firebase.firestore.QuerySnapshot;
+//import java.util.ArrayList;
+//
+////AddHabitEventFragment.OnFragmentInteractionListener
+//public class EventsFragment extends Fragment{
+//
+//    ListView EventsList;
+//    ArrayAdapter<HabitEvents> eventAdapter;
+//    ArrayList<HabitEvents> eventsDataList;
+//
+//    public EventsFragment() {
+//        // Required empty public constructor
+//    }
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_events, null);
+//
+//        EventsList = view.findViewById(R.id.eventList); // To display the Habit Events lists
+//
+//        eventsDataList = new ArrayList<>();
+//
+//
+//        // TODO: Need to check if it's getContext or getActivity
+//
+//        eventAdapter = new EventsAdapter(getContext(), eventsDataList);
+//
+//        EventsList.setAdapter(eventAdapter);
+//
+//
+//        FirebaseFirestore HabTrackDB = FirebaseFirestore.getInstance();
+//        HabTrackDB.collection("Users")
+//                .document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+//                .collection("HabitEvents")
+//                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+//                        eventsDataList.clear();
+//                        for(QueryDocumentSnapshot hevent: value) {
+//                            HabitEvents hEvent = hevent.toObject(HabitEvents.class);
+////                            eventsDataList.add(hEvent);
+//                            eventAdapter.add(hEvent);
+//                        }
+//                        eventAdapter.notifyDataSetChanged();
+//                    }
+//                });
+//
+//        EventsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//
+//                HabitEvents currentEvent = eventAdapter.getItem(i);
+//
+//                new ViewEditDeleteHabitEvent(currentEvent);
+//            }
+//        });
+//
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_events, container, false);
+//    }
+//
+//}
+//
